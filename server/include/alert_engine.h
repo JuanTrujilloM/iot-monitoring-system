56359a4481f46325d03f823a6d79591516f75103
JuanTrujilloM <juantru2211@gmail.com>
2026-04-01 19:16:52 -0500

    Principal Structure for the project

--- server/include/alert_engine.h
+++
@@ -1,8 +0,0 @@
-#ifndef ALERT_ENGINE_H
-#define ALERT_ENGINE_H
-
-/* TODO: definir umbrales por tipo de sensor como constantes */
-/* TODO: declarar alert_evaluate, alert_broadcast,
-         alert_register_operator, alert_unregister_operator  */
-
-#endif /* ALERT_ENGINE_H */
--- server/include/logger.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOGGER_H
-#define LOGGER_H
-
-typedef enum { LOG_INFO, LOG_WARN, LOG_ERROR } LogLevel;
-
-/* TODO: declarar logger_init, log_info, log_warn, log_error, logger_close */
-
-#endif /* LOGGER_H */
